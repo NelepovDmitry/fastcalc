58e92611356fee7c2984e617c0bede03ebafaf1c
Gevorg Petrosyan <petrosyan@MacMiniGevorg.local>
2012-04-27 10:33:30 +0400

    Проект создан

---
+++ fastcalc/fastcalc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  fastcalc
+//
+//  Created by Gevorg Petrosyan on 27.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ fastcalc/fastcalc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  fastcalc
+//
+//  Created by Gevorg Petrosyan on 27.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
