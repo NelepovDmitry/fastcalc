4510d5b8abc35985afc4894cb68e8100623969f4
Gevorg Petrosyan <petrosyan@MacMiniGevorg.local>
2012-07-05 18:24:06 +0400

    Добавил класс для инфы

---
+++ fastcalc/FastCalc/FastCalc/InfoController.h
@@ -0,0 +1,13 @@
+//
+//  InfoController.h
+//  FastCalc
+//
+//  Created by Gevorg Petrosyan on 05.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoController : UIViewController
+
+@end
